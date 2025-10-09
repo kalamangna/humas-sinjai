182183df8cb10964ff76309ee932700438cd5a01
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2025-10-09 13:11:06 +0800

    fix image url in editor

--- app/Views/layout/admin_header.php
+++ app/Views/layout/admin_header.php
@@ -25,12 +25,15 @@
     </script>
     <script src="https://cdn.tiny.cloud/1/d0biv5p4ggrqsdl9idntd4aoeda7o4wcf12azjs82dojzezj/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
-              tinymce.init({
-                selector: 'textarea#content',
-                plugins: 'code table lists image',
-                toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table | image',
-                images_upload_url: '<?= base_url('admin/posts/upload_image') ?>',
-              });    </script>
+        tinymce.init({
+            selector: 'textarea#content',
+            plugins: 'code table lists image',
+            toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table | image',
+            images_upload_url: '<?= site_url('admin/posts/upload_image') ?>',
+            relative_urls: false,
+            remove_script_host: false,
+        });
+    </script>
 </head>
 
 <body class="bg-light">
