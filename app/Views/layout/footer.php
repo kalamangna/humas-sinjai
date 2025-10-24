a984c07abf67f46d31efc9d483bb5c7fcf3eb514
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2025-10-24 15:02:51 +0800

    change footer logo

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -3,10 +3,10 @@
     <div class="container py-5">
         <div class="row g-3">
             <!-- Brand info -->
-            <div class="col-lg-4 mb-4 text-center text-lg-start">
-                <div class="d-block d-lg-flex align-items-center justify-content-center justify-content-lg-start mb-3">
-                    <img src="<?= base_url('logo.png') ?>" alt="Logo Sinjai" width="50" height="50" class="me-lg-3 mb-2 mb-lg-0">
-                    <h5 class="fw-bold mb-0 text-white">Humas Sinjai</h5>
+            <div class="col-md-12 col-lg-4 mb-4">
+                <div class="d-block mb-3">
+                    <img src="<?= base_url('humas.png') ?>" alt="Logo Sinjai" height="50">
+                    <!-- <h5 class="fw-bold mb-0 text-white">Humas Sinjai</h5> -->
                 </div>
                 <p class="text-light mb-2">
                     Portal Berita Resmi Pemerintah Kabupaten Sinjai
@@ -14,59 +14,59 @@
                 <span class="badge bg-light text-primary mb-3">
                     <i class="fas fa-hashtag me-1"></i>samasamaki
                 </span>
-                <div class="d-flex gap-3 justify-content-center justify-content-lg-start">
+                <div class="d-flex gap-3">
                     <a href="https://www.facebook.com/FP.KabupatenSinjai" target="_blank" class="text-white fs-5"><i class="fab fa-facebook"></i></a>
-                    <a href="https://x.com/sinjaikab" target="_blank" class="text-white fs-5"><i class="fab fa-x-twitter"></i></a>
                     <a href="https://www.instagram.com/sinjaikab" target="_blank" class="text-white fs-5"><i class="fab fa-instagram"></i></a>
-                    <a href="https://www.youtube.com/@SINJAITV" target="_blank" class="text-white fs-5"><i class="fab fa-youtube"></i></a>
                     <a href="https://www.tiktok.com/@pemkabsinjai" target="_blank" class="text-white fs-5"><i class="fab fa-tiktok"></i></a>
+                    <a href="https://x.com/sinjaikab" target="_blank" class="text-white fs-5"><i class="fab fa-x-twitter"></i></a>
+                    <a href="https://www.youtube.com/@SINJAITV" target="_blank" class="text-white fs-5"><i class="fab fa-youtube"></i></a>
                 </div>
             </div>
 
             <!-- Quick links -->
-            <div class="col-lg-2 mb-4 text-center text-lg-start">
+            <div class="col-md-4 col-lg-2 mb-4">
                 <h6 class="fw-bold mb-3 text-white">Menu</h6>
                 <ul class="list-unstyled">
                     <li class="mb-2">
                         <a href="<?= base_url('/') ?>" class="text-light text-decoration-none">
-                            <i class="fas fa-arrow-right me-2 small text-white"></i>Beranda
+                            <i class="fas fa-fw fa-arrow-right me-2 small text-white"></i>Beranda
                         </a>
                     </li>
                     <li class="mb-2">
                         <a href="<?= base_url('about') ?>" class="text-light text-decoration-none">
-                            <i class="fas fa-arrow-right me-2 small text-white"></i>Tentang
+                            <i class="fas fa-fw fa-arrow-right me-2 small text-white"></i>Tentang
                         </a>
                     </li>
                     <li class="mb-2">
                         <a href="<?= base_url('contact') ?>" class="text-light text-decoration-none">
-                            <i class="fas fa-arrow-right me-2 small text-white"></i>Kontak
+                            <i class="fas fa-fw fa-arrow-right me-2 small text-white"></i>Kontak
                         </a>
                     </li>
                     <li class="mb-2">
                         <a href="<?= base_url('rss') ?>" class="text-light text-decoration-none">
-                            <i class="fas fa-rss me-2 small text-white"></i>RSS
+                            <i class="fas fa-fw fa-rss me-2 small text-white"></i>RSS
                         </a>
                     </li>
                 </ul>
             </div>
 
             <!-- Contact info -->
-            <div class="col-lg-3 mb-4 text-center text-lg-start">
+            <div class="col-md-8 col-lg-3 mb-4">
                 <h6 class="fw-bold mb-3 text-white">Kontak</h6>
                 <ul class="list-unstyled text-light">
-                    <li class="mb-3 d-flex align-items-start justify-content-center justify-content-lg-start">
-                        <i class="fas fa-envelope me-3 mt-1 text-white"></i>
+                    <li class="mb-3">
+                        <i class="fas fa-fw fa-envelope me-2 text-white"></i>
                         <span>humas@sinjaikab.go.id</span>
                     </li>
-                    <li class="mb-3 d-flex align-items-start justify-content-center justify-content-lg-start">
-                        <i class="fas fa-map-marker-alt me-3 mt-1 text-white"></i>
-                        <span>Jl. Persatuan Raya No. 101, Kel. Balangnipa, Kec. Sinjai Utara, Kab. Sinjai</span>
+                    <li class="mb-3">
+                        <i class="fas fa-fw fa-map-marker-alt me-2 text-white"></i>
+                        <a href="https://maps.app.goo.gl/8WX2QetWMMaGdYiw5" target="_blank" class="text-light text-decoration-none">Jl. Persatuan Raya No. 101, Kel. Balangnipa, Kec. Sinjai Utara, Kab. Sinjai</a>
                     </li>
                 </ul>
             </div>
 
             <!-- Banner -->
-            <div class="col-lg-3 mb-4">
+            <div class="col-md-12 col-lg-3 mb-4">
                 <img src="<?= base_url('poster.png') ?>" alt="Banner" class="img-fluid">
             </div>
         </div>
