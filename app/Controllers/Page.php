252213f8a103c5e05b78cacc342d6a4b2f0644b0
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2025-11-14 17:40:52 +0800

    Revert "Feat: Add meta image and description to widget page"
    This reverts commit 55c896b1b296b70d37d52b0ab6574b5885e1c8ec.

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -19,8 +19,6 @@ class Page extends BaseController
     public function widget()
     {
         $data['title'] = 'Panduan Widget';
-        $data['description'] = 'Panduan lengkap untuk memasang widget berita Humas Sinjai di situs web Anda. Tampilkan berita terbaru dengan mudah.';
-        $data['image'] = base_url('meta.png');
         return view('widget_guide', $data);
     }
 }
